a69545ad91c709f90d63c42b23be6b8aa65d35f0
htmiguel <miguel.senquiz@gmail.com>
2013-03-12 17:19:52 -0400

    updated instagram api.

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,77 @@
 	}
 	
 })(jQuery);
+
+
+
+//INSTAGRAM PLUGIN
+(function($){
+  $.fn.instagram = function(options) {
+
+		var defaults = [];
+		var options = $.extend(defaults, options);		
+		var access_token = '22498578.f59def8.59f50138a23b49208ccf3590e28ffe14';
+		var instagramURL = "https://api.instagram.com/v1/users/"+options.hash+"/media/recent?access_token=" + access_token;
+
+		loadPhotos = function() {
+			var next_url;
+			if (instagramURL !== undefined) {
+				$.ajax({
+					type: "GET",
+					dataType: "jsonp",
+					cache: false,
+					url: instagramURL,
+					success: function(data) {
+
+						for(var i = 0; i < 5; i++) {
+
+							var $item = 
+							$('<li />')
+								.attr('id', data.data[i].id)
+					      .append(
+									$('<a>')
+										.attr('href', data.data[i].link)
+										.attr('rel', "shadowbox[Instagrams];title=<span>photo by @</span>" + data.data[i].user.username)
+										.append(
+											$('<div>')
+										  	.addClass('insta_thumb')
+										    .append(
+							             $('<img>')
+							               .addClass('instagram-image')
+							               .attr('src', data.data[i].images.low_resolution.url)
+							           )
+										)
+										.append(
+											$('<div>')
+										  	.addClass('insta_expand')
+												// .append(
+												// 	$('<span>')
+												// 		.addClass('likes_count')
+												// 		.html('&hearts;'+data.data[i].likes.count)
+												// )
+										)
+									);
+
+
+							$item.hide().appendTo($('#instas')).fadeIn();
+						}
+
+						next_url = data.pagination.next_url;
+						instagramURL = next_url;
+
+						// Shadowbox.init({ skipSetup:true,overlayOpacity:0.80 }); 
+						// Shadowbox.setup();
+
+						// $('button').one('click', function(){
+						// 	loadPhotos();
+						// });						
+
+					}
+				});
+			}
+		}
+
+		loadPhotos();
+
+  };
+})(jQuery);
